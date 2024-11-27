42e26368ce3a91abb0cea3a8cc03e4b49affbf94
Gabi_Prajo <prajo.gabi@gmail.com>
2024-11-26 22:02:13 -0300

    alinhamento <p> fimAgendamento

--- fimAgendamento.php
+++ fimAgendamento.php
@@ -16,12 +16,14 @@
         <h1>DADOS DA CONSULTA</h1>
         <div class="conteudo">
             <h2>Confira seus dados:</h2>
-            <p><strong>Paciente:</strong> Francisco Veiga</p>
-            <p><strong>Profissional:</strong> Dentista</p>
-            <p><strong>Especialidade:</strong> Ana Paula Silva</p>
-            <p><strong>Data da consulta:</strong> 02/02/2025</p>
-            <p><strong>Horário:</strong> 09:00</p>
-            <p><strong>Unidade:</strong> UBS Feu Rosa</p>
+            <div class="esquerda">
+                <p><strong>Paciente:</strong> Francisco Veiga</p>
+                <p><strong>Profissional:</strong> Dentista</p>
+                <p><strong>Especialidade:</strong> Ana Paula Silva</p>
+                <p><strong>Data da consulta:</strong> 02/02/2025</p>
+                <p><strong>Horário:</strong> 09:00</p>
+                <p><strong>Unidade:</strong> UBS Feu Rosa</p>
+            </div>
             <div class="centralizar">
                 <button type="button" class="voltarbtn" id="btns" onclick="history.back()">Voltar</button>
                 <a class="botao" href="pagPrincipal.php" id="btns">Finalizar Agendamento</a>
