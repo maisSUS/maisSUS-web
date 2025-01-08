8ea053efe2cd9db6d978fcc1c082d274ee9b5fcf
Gabi_Prajo <prajo.gabi@gmail.com>
2025-01-08 14:39:42 -0300

    conexão dando erro de charset

--- cadastrarUsuario.php
+++ cadastrarUsuario.php
@@ -1,31 +1,31 @@
 <?php
-session_start();
-if (!isset($_SESSION['dados_form'])) {
-    die('Acesso inválido!');
-}
+    session_start();
+    if (!isset($_SESSION['dados_form'])) {
+        die('Acesso inválido!');
+    }
 
-$dados = $_SESSION['dados_form'];
-unset($_SESSION['dados_form']); // Limpa a sessão para evitar reenvios
+    $dados = $_SESSION['dados_form'];
+    unset($_SESSION['dados_form']); // Limpa a sessão para evitar reenvios
 
-// Conexão e inserção no banco
-require_once 'database.php';
-try{
-    $sql = "INSERT INTO LOGIN_USUARIO (nome, cpf, cartaoSus, telefone, email, senha) VALUES (:nome, :cpf, :cartaoSus, :telefone, :email, :senha)";
-    $stmt = Database::prepare($sql);
-    $stmt->bindParam(':nome', $dados['nome']);
-    $stmt->bindParam(':cpf', $dados['cpf']);
-    $stmt->bindParam(':cartaoSus', $cartaoSus);
-    $stmt->bindParam(':telefone', $telefone);
-    $stmt->bindParam(':email', $email);
-    $hashedSenha = password_hash($senha, PASSWORD_DEFAULT);
-    $stmt->bindParam(':senha', $hashedSenha);
-    $stmt->execute();
+    // Conexão e inserção no banco
+    require_once 'database.php';
+    try{
+        $sql = "INSERT INTO LOGIN_USUARIO (nome, cpf, cartaoSus, telefone, email, senha) VALUES (:nome, :cpf, :cartaoSus, :telefone, :email, :senha)";
+        $stmt = Database::prepare($sql);
+        $stmt->bindParam(':nome', $dados['nome']);
+        $stmt->bindParam(':cpf', $dados['cpf']);
+        $stmt->bindParam(':cartaoSus', $cartaoSus);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':email', $email);
+        $hashedSenha = password_hash($senha, PASSWORD_DEFAULT);
+        $stmt->bindParam(':senha', $hashedSenha);
+        $stmt->execute();
 
-    $idUsuario = Database::getInstance()->lastInsertId();
-    $_SESSION['idUsuario'] = $idUsuario;
-    echo "Usuário cadastrado com sucesso!";
-} catch(PDOException $e) {
-    echo "Erro ao cadastrar o usuário: " . $e->getMessage();
-}
+        $idUsuario = Database::getInstance()->lastInsertId();
+        $_SESSION['idUsuario'] = $idUsuario;
+        echo "Usuário cadastrado com sucesso!";
+    } catch(PDOException $e) {
+        echo "Erro ao cadastrar o usuário: " . $e->getMessage();
+    }
 
 ?>
